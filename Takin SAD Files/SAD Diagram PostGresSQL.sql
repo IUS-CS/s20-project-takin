14dbac33537577ecd91de8f511e0ba6c38bee314
haasAC <hasac@live.com>
2020-02-27 02:48:30 -0500

    Add files via upload
    Adding a folder with the SAD diagram included. Not Completely sure if thats what we need because I'm not sure about the table names and what they contain. But this folder contains a PNG and a PDF of the diagram. and an SQL version of it too.

---
+++ Takin SAD Files/SAD Diagram PostGresSQL.sql
@@ -0,0 +1,25 @@
+CREATE TABLE "SEDeli" (
+  "VeggieWrap" varchar,
+  "CaesarWrap" varchar,
+  "FiestaFajitaWrap" varchar,
+  "ChickenCaesarWrap" varchar
+);
+
+CREATE TABLE "Grill" (
+  "ChickenTenders" varchar,
+  "VeggieBurgerBasket" varchar,
+  "GrilledChickenSandwichBasket" varchar,
+  "QuarterPoundCheeseburgerBasket" varchar
+);
+
+CREATE TABLE "Breakfast" (
+  "BreakfastBurrito" varchar,
+  "BigBreakfast" varchar,
+  "PancakeBreakfast" varchar,
+  "OmeletBreakfast" varchar
+);
+
+CREATE TABLE "Salads" (
+  "TraditionalTunaSalad" varchar,
+  "ClassicChickenSalad" varchar
+);
